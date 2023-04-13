a9306f809e5da1c28eb436287299a8019d17f64c
see92 <dsq_1105@outlook.com>
2023-04-13 11:59:01 +0800

    update

--- src/views/softwareList/index.vue
+++ src/views/softwareList/index.vue
@@ -3,13 +3,14 @@
     <b-overlay :show="show">
       <!-- 头部 -->
       <div class="d-flex justify-content-between mt-2 ml-2">
-        <b-form class="d-flex">
-          <b-form-input class="w25 mr-1" placeholder="请输入需要查询的软件名称" />
-          <b-button variant="success" class="mr-1"> <feather-icon icon="SearchIcon" /> 查询</b-button>
-          <b-button variant="info"> <feather-icon icon="UploadIcon" /> 下载软件规则 </b-button>
-        </b-form>
+        <div class="d-flex">
+          <b-form-input autocomplete="off" v-model="searchVal" class="w25 mr-1" placeholder="请输入需要查询的软件名称" @keyup.enter="searchInfo" />
+          <b-button variant="success" class="mr-1" @click="searchInfo"> <feather-icon icon="SearchIcon" /> 查询</b-button>
+          <b-button variant="info" @click="uploadFile"> <feather-icon icon="UploadIcon" /> 下载软件规则 </b-button>
+          <input type="file" v-show="false" ref="fileRef" @change="uploadFileWithClear($event)">
+        </div>
         <div class="mr-2">
-          <b-button variant="primary"> <feather-icon icon="PlusIcon" /> 添加</b-button>
+          <b-button variant="primary"> <feather-icon icon="PlusIcon" @click="isButton(0)" /> 添加</b-button>
         </div>
       </div>
 
@@ -21,8 +22,42 @@
               暂无数据
             </div>
           </template>
+          <!-- 图片加载 -->
           <template #cell(icon)="data">
             <img :src="`data:image/jpg;base64,${data.item.icon}`" style="width: 30px; height: 30px">
+          </template>
+          <!-- 付费类型 -->
+          <template #cell(payment_type)="data">
+            <b-badge pill :variant="PaymentVariant[data.item.payment_type]">
+              {{ findLabel(payOptions,data.item.payment_type) }}
+            </b-badge>
+          </template>
+          <!-- 级别 -->
+          <template #cell(positive_level)="data">
+            {{ findLabel(lvOptions,data.item.positive_level) }}
+          </template>
+          <!-- 过滤流量 -->
+          <template #cell(filter_traffic)="data">
+            {{ findLabel(flowOptions,data.item.filter_traffic) }}
+          </template>
+          <!-- 涉密类型 -->
+          <template #cell(leak_type)="data">
+            {{ findLabel(leakTypeOptions,data.item.leak_type) }}
+          </template>
+          <!-- 透明加解密 -->
+          <template #cell(support_des)="data">
+            <b-badge :variant="supportVariant[data.item.support_des]">
+              {{ data.item.support_des == 1? '支持' :'不支持' }}
+            </b-badge>
+          </template>
+          <!-- 操作 -->
+          <template #cell(actions)="data">
+            <div class="d-flex">
+              <feather-icon icon="EditIcon" :id="`Edit_${data.index}`" style="cursor:pointer" class="mr-50" @click="isButton(1,data.item)" />
+              <b-tooltip :target="`Edit_${data.index}`" title="编辑信息" placement="left" />
+              <feather-icon icon="Trash2Icon" :id="`Trash2_${data.index}`" style="cursor:pointer" @click="isButton(2,data.item)" />
+              <b-tooltip :target="`Trash2_${data.index}`" title="删除信息" placement="left" />
+            </div>
           </template>
         </b-table>
       </div>
@@ -35,17 +70,29 @@
           <div class="d-flex align-items-baseline">
             <!-- <input type="number" style="width:50px" /> -->
             <span class="mr-50">前往</span>
-            <b-form-input style="height:32px;width:50px" class="mr-50" />
+            <b-form-input type="number" autocomplete="off" style="height:32px;width:50px" class="mr-50" v-model="pageNum" @keyup.enter="goPage" />
             <span class="mr-2">页</span>
           </div>
         </div>
+      </div>
+
+      <!-- 弹窗 -->
+      <div>
+        <b-modal centered hide-footer no-close-on-backdrop v-model="isDelete" size="sm" title="提示：">
+          <div class="d-flex justify-content-center">此操作将会删除数据，是否继续？</div>
+          <div class="d-flex mt-1 justify-content-end">
+            <b-button variant="danger" class="mr-1">确定</b-button>
+            <b-button variant="outline-danger">取消</b-button>
+          </div>
+        </b-modal>
       </div>
     </b-overlay>
   </b-card>
 </template>
 
 <script>
-import { softwareList } from "./js/api";
+import { softwareList,offlineJson } from "./js/api";
+import { payOptions, lvOptions, flowOptions, leakTypeOptions } from "./js/options";
 export default {
   name: 'softwareList',
   components: {},
@@ -71,13 +118,82 @@ export default {
       page: 1,
       size: 10,
       count: 0,
-      searchVal: ''
+      searchVal: '',
+      pageNum: '',
+      // 下拉选项
+      payOptions,
+      lvOptions,
+      flowOptions,
+      leakTypeOptions,
+      // 高亮颜色
+      PaymentVariant: { 1: "success", 2: "danger", 3: "warning", 4: "info" },
+      supportVariant: { 0: "danger", 1: "success" },
+      isDelete: false,  //删除弹窗
     }
   },
   mounted() {
     this.getSoftwareList()
   },
   methods: {
+    // 上传文件
+    uploadFile() {
+      this.$refs.fileRef.dispatchEvent(new MouseEvent('click'))
+    },
+    async uploadFileWithClear(e) {
+      let file = e.target.files[0]
+      let formData = new FormData()
+      formData.append('file', file)
+      try {
+        const data = await offlineJson(formData)
+        let blob = new Blob([data.data], { type: 'application/octet-stream' });
+        let url = window.URL.createObjectURL(blob)
+        // return
+        let link = document.createElement('a');
+        link.download = 'software_rule.json';
+        link.href = url;
+        link.click()
+        //释放内存
+        window.URL.revokeObjectURL(link.href)
+      } catch (error) {
+      }
+      this.$refs.fileRef.value = "";
+    },
+    // 点击页码
+    changePage(e) {
+      this.page = e;
+      this.getSoftwareList()
+    },
+    // 回显下拉内容
+    findLabel(params, id) {
+      let items = [];
+      params.forEach((item) => {
+        if (item.id === id) {
+          items.push(item.name)
+        }
+      });
+      return items.toString()
+    },
+    // 按钮操作
+    isButton(flag, data) {
+      if (flag == 0) {
+
+      } else if (flag == 1) {
+
+      } else if (flag == 2) {
+        this.isDelete = true
+      }
+    },
+    // 查询操作
+    searchInfo() {
+      this.page = 1
+      this.getSoftwareList()
+    },
+    // 跳转指定页码
+    goPage() {
+      this.page = this.pageNum;
+      this.getSoftwareList()
+    },
+    // 请求软件列表
     async getSoftwareList() {
       this.show = true;
       try {
@@ -88,17 +204,11 @@ export default {
         })
         this.count = data.count
         this.isItems = data.data
-        console.log(data, 'data');
       } catch (error) {
 
       }
       this.show = false
     },
-    // 点击页码
-    changePage(e) {
-      this.page = e;
-      this.getSoftwareList()
-    }
   },
 }
 </script>
@@ -113,7 +223,7 @@ export default {
 
 <style lang="less">
 .tableTr td {
-  max-width: 10rem;
+  max-width: 9rem;
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
--- src/views/softwareList/js/api.js
+++ src/views/softwareList/js/api.js
@@ -7,3 +7,13 @@ export const softwareList = (params) => {
     params,
   });
 };
+
+// 上传文件信息
+export const offlineJson = (data) => {
+  return axios.request({
+    url: "software_callout/get_offline_json",
+    method: "post",
+    data,
+    responseType: "blob",
+  });
+};
